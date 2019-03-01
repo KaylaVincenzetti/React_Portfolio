59170702051e0afb3b4c484b75a168d0f4e0ab53
KayVin <kaylavincenzetti@MiniMac.local>
2019-02-28 21:38:17 -0700

    Added app-title and introduction paragraph

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my React Portfolio</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi! I'm Kayla Vincenzetti and I'm a full stack developer, freelancer and teacher based in Denver, Colorado.
           </p>
           <a
             className="App-link"
